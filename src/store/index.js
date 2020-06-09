598f6e2be680268ae11ee349df5b6336292dfa70
Julien Aupart <julien.aupart@gmail.com>
2020-06-09 18:42:35 +0200

    Ramdomize cards before starting quiz

--- src/store/index.js
+++ src/store/index.js
@@ -9,12 +9,46 @@ export default new Vuex.Store({
   },
 
   state: {
-    cards: [],
+    cards: [
+      {
+        question: 'Question #1',
+        rightAnswer: 'Right answer',
+        wrongAnswer1: 'Wrong Answer #1',
+        wrongAnswer2: 'Wrong Answer #2',
+      },
+      {
+        question: 'Question #2',
+        rightAnswer: 'Right answer',
+        wrongAnswer1: 'Wrong Answer #1',
+        wrongAnswer2: 'Wrong Answer #2',
+      },
+      {
+        question: 'Question #3',
+        rightAnswer: 'Right answer',
+        wrongAnswer1: 'Wrong Answer #1',
+        wrongAnswer2: 'Wrong Answer #2',
+      },
+      {
+        question: 'Question #4',
+        rightAnswer: 'Right answer',
+        wrongAnswer1: 'Wrong Answer #1',
+        wrongAnswer2: 'Wrong Answer #2',
+      },
+      {
+        question: 'Question #5',
+        rightAnswer: 'Right answer',
+        wrongAnswer1: 'Wrong Answer #1',
+        wrongAnswer2: 'Wrong Answer #2',
+      },
+    ],
   },
 
   mutations: {
     addNewCard(state, card) {
       state.cards.push(card)
+    },
+    shuffleCards(state) {
+      shuffleArray(state.cards)
     },
   },
 
@@ -22,5 +56,16 @@ export default new Vuex.Store({
     addNewCardAsync({ commit }, card) {
       commit('addNewCard', card)
     },
+
+    shuffleCardsAsync({ commit }) {
+      commit('shuffleCards')
+    },
   },
 })
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+}
--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,3 +1,25 @@
 <template>
   <div></div>
 </template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'Quiz',
+
+  computed: {
+    ...mapGetters(['cards']),
+  },
+
+  beforeMount() {
+    this.shuffleCardsAsync()
+  },
+
+  methods: {
+    ...mapActions(['shuffleCardsAsync']),
+  },
+}
+</script>
+
+<style></style>
