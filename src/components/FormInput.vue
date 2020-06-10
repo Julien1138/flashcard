fc65c5e07e3a054e8cf1df0397c9b70db0ac7690
Julien Aupart <julien.aupart@gmail.com>
2020-06-10 11:18:45 +0200

    Restore FormInput

---
+++ src/components/FormInput.vue
@@ -0,0 +1,41 @@
+<template>
+  <input
+    v-model="newValue"
+    :class="isInvalid ? 'is-invalid' : ''"
+    :autofocus="autofocus"
+    @keyup="onChange"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      default: '',
+    },
+    autofocus: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  data() {
+    return {
+      newValue: this.value,
+      isInvalid: false,
+    }
+  },
+
+  methods: {
+    onChange() {
+      this.isInvalid = false
+      this.$emit('input', this.newValue)
+    },
+  },
+}
+</script>
